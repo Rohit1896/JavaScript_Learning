0151ab046f41bc579dfddd145f7cb1ca627c6a65
HP <rrborade4@gmail.com>
2025-08-19 22:22:50 +0530

    added new assingment

---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,36 @@
+
+console.log("**************Assignment 01*****************");
+console.log("===================Part 01=================");
+console.log("========String Variables=========");
+let firstName = "Rohit";
+console.log('First Name:-',firstName,"      " , "Variable Type:-",typeof(firstName));
+let lastName = "Borade";
+console.log('Last Name:-',lastName,"       " , "Variable Type:-",typeof(lastName));
+let address = "Pune";
+console.log('Address:-',address,"             " , "Variable Type:-",typeof(address));
+
+console.log("================= Number Variable ===================");
+let pinCode = 121212;
+console.log('Pincode:-',pinCode,"            " , "Variable Type:-",typeof(pinCode));
+var streetNumber = 7;
+console.log('Street Number:-',streetNumber,"          " , "Variable Type:-",typeof(streetNumber));
+var distance=25.5;
+console.log('Distance:-',distance,"             " , "Variable Type:-",typeof(distance));
+
+console.log("========Boolean Variables=========");
+var isAvailable=true;
+console.log('Availabality Status:-',isAvailable,"        " , "Variable Type:-",typeof(isAvailable));
+var isEmployed=true;
+console.log('Employability Status:-',isEmployed,"       " , "Variable Type:-",typeof(isEmployed));
+var isSick=false;
+console.log('Health Condition:-',isSick,"          " , "Variable Type:-",typeof isSick);
+
+console.log("=======Difference between null and undefined=======");
+console.log('================Null=====================')
+var amount=null;
+console.log('Amount:-',amount,"           ","Variable Type:-", typeof amount);
+console.log("Null:- null is something but not zero or undefined. It is required to assign null but not undefined");
+console.log('====================== Undefined==================')
+var height;
+console.log('Height:-',height,"           ", "Variable Type:-",typeof height);
+console.log("Undefined:- undefined garbage value stored in variable after declaration only.")
